b8e57ad00b871fc42af8f2086c0e23e3bab6374f
João Neto C <j.neto111@gmail.com>
2023-08-31 08:38:27 -0300

    feat: use env

--- src/sendDiscordMessage.ts
+++ src/sendDiscordMessage.ts
@@ -1,5 +1,5 @@
 export async function sendDiscordMessage(body: object){
-    const url = 'https://discord.com/api/webhooks/1146768473163317299/jvMUj5bsKLtchtJ5jMPufORl7adUJO1Yncn4tcDJsTeiSqs_Dwdu8ZnbGkQe5QhNDhDj'
+    const url = `${process.env.DISCORD_WEBHOOK_URL}`
     await fetch(url, {
         body: JSON.stringify(body),
         method: 'POST',
